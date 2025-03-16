5bf584ded3f9c37c94a023bec9fb16c9c3271565
Initha-Derin <inithaderin@gmail.com>
2025-03-16 23:33:54 +0530

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ListofCourses from "../ListofCourses/listofcourses";
+
+class App extends React.Component {
+  render() {
+    return (
+      
+        <ListofCourses />
+   
+      );
+  }
+}
+
+export default App;
